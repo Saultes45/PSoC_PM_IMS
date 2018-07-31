e753682fb7dab7f8339a8ee2b95052e336dc06be
Nathan <nathanael.esnault@gmail.com>
2018-08-01 09:33:37 +1200

    correction of data

--- SimuSensorData.h
+++ SimuSensorData.h
@@ -11,22 +11,19 @@
 */
 
 //False data reading, for sensor emulation
-#define WI_BOW_CURRENTDATA	        1 //Digital 0/1
-#define WI_STERN_CURRENTDATA	    1 //Digital 0/1
-#define WI_PORT_CURRENTDATA	        1 //Digital 0/1
-#define WI_STARBOARD_CURRENTDATA	1 //Digital 0/1
-#define BALLSW_BOW_CURRENTDATA	        1 //Digital 0/1
-#define BALLSW_STERN_CURRENTDATA	    1 //Digital 0/1
-#define BALLSW_PORT_CURRENTDATA	        1 //Digital 0/1
-#define BALLSW_STARBOARD_CURRENTDATA	1 //Digital 0/1
-#define BATTERY_VOLATGE_CURRENTDATA	12.4  //Volts
-#define BATTERY_AMPERAGE_CURRENTDATA	0.0153  //l/s
-#define THEMAL_PORT_CURRENTDATA	        1 //Digital 0/1
-#define THEMAL_STARTBOARD_CURRENTDATA	        1 //Digital 0/1
-#define THEMAL_CENTER_CURRENTDATA	        1 //Digital 0/1
-#define WC_FLOWRATE_CURRENTDATA	0.0153  //l/s
-#define TACOM_STARTBOARD_CURRENTDATA	        1 //Digital 0/1
-#define TACOM_PORT_CURRENTDATA	        1 //Digital 0/1
+#define WI_BOW_CURRENTDATA	        		1 //Digital 0/1
+#define WI_STERN_CURRENTDATA	    		1 //Digital 0/1
+#define WI_PORT_CURRENTDATA	        		1 //Digital 0/1
+#define WI_STARBOARD_CURRENTDATA			1 //Digital 0/1
+#define BATTERY_VOLATGE_CURRENTDATA			12.4  //Volts
+#define BATTERY_AMPERAGE_CURRENTDATA		0.0153  //l/s
+#define THEMAL_PORT_CURRENTDATA	        	35.32 //Digital 0/1
+#define THEMAL_STARTBOARD_CURRENTDATA	    30.7 //Digital 0/1
+#define THEMAL_CENTER_CURRENTDATA	        24.15 //Digital 0/1
+#define WC_FLOWRATE_CURRENTDATA				0.0153  //l/s
+#define TACOM_STARTBOARD_CURRENTDATA	    27.3 // rotation/s
+#define TACOM_PORT_CURRENTDATA	        	30.1 //rotation/s
 
 
 /* [] END OF FILE */
+
